6974d48ea6f296548b77a03e8d4e359172e6358d
骆尚昂 <shangang_luo@fpi-inc.com>
2020-12-20 15:50:54 +0800

    first commit

---
+++ src/main/java/com/lz/bmp/UserOperateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lz.bmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserOperateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserOperateServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lz/bmp/UserOperateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.lz.bmp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class UserOperateServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
